48aedbc40d60f34d78127803f38660c3b620d42f
Neo Zhuo <neowiz@icloud.com>
2021-04-25 12:27:04 +0800

    fix: update test case

--- http_test.go
+++ http_test.go
@@ -2,6 +2,7 @@ package libprobe_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/blho/libprobe"
 
@@ -11,8 +12,16 @@ import (
 func TestHTTPProber(t *testing.T) {
 	result, err := libprobe.NewHTTPProber().Probe(libprobe.Target{
 		Address: "https://baidu.com",
-		Timeout: 0,
+		Timeout: 3 * time.Second,
 	})
 	require.NoError(t, err)
 	t.Logf("Result: \n%v", result)
+
+	result, err = libprobe.NewHTTPProber().Probe(libprobe.Target{
+		Address: "https://google.com",
+		Timeout: 3 * time.Second,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Error(t, result.(*libprobe.HTTPResult).Error)
 }
